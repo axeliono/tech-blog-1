df842c063b24a1d2cfa50fe881d4661e2b1889a1
tmstafford <tatumstafford1@gmail.com>
2021-01-14 10:56:42 -0600

    comment functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,16 +13,18 @@ router.get('/', (req, res) => {
 
 // create a comment /api/comments
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        post_id: req.body.post_id,
-        user_id: req.body.user_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
 });
 
 // delete a comment /api/comments/1
